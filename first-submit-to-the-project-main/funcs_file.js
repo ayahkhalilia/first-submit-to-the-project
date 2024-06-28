713fe84245f54c1265420ac2ffc5bd971f9e70d4
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-06-28 18:54:08 +0300

    added the fetch data from json function but i have to make the data clickable

--- first-submit-to-the-project-main/funcs_file.js
+++ first-submit-to-the-project-main/funcs_file.js
@@ -1,1 +1,28 @@
+window.onload = () =>{
+  fetch_data();
+}
 
+function fetch_data() {
+    fetch("data_file.json")
+         .then(response =>response.json())
+         .then(data =>{
+            const wrapper_list=document.getElementById("list-rec1");
+            const title=document.createElement("h1");
+            title.innerHTML=data.category;
+            wrapper_list.appendChild(title);
+           
+            const list=document.createElement("ul");
+            data.requests.forEach(request => {
+                const li=document.createElement("li");
+                const img = document.createElement("img");
+                img.src = request.img;
+                const textContent = document.createElement("div");
+                textContent.innerHTML = `${request.name}<br><br>${request.detail}`;
+
+                li.appendChild(img);
+                li.appendChild(textContent);
+                list.appendChild(li);
+            });
+            wrapper_list.appendChild(list);
+         });
+}
