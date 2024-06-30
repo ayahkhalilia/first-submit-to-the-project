2c3f25a647ac63c0dab0094625571649633780e7
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-06-30 04:43:43 +0300

    added fetch link from json

--- first-submit-to-the-project-main/funcs_file.js
+++ first-submit-to-the-project-main/funcs_file.js
@@ -4,25 +4,53 @@ window.onload = () =>{
 
 function fetch_data() {
     fetch("data_file.json")
-         .then(response =>response.json())
-         .then(data =>{
-            const wrapper_list=document.getElementById("list-rec1");
-            const title=document.createElement("h1");
-            title.innerHTML=data.category;
-            wrapper_list.appendChild(title);
-           
-            const list=document.createElement("ul");
-            data.requests.forEach(request => {
-                const li=document.createElement("li");
-                const img = document.createElement("img");
-                img.src = request.img;
-                const textContent = document.createElement("div");
-                textContent.innerHTML = `${request.name}<br><br>${request.detail}`;
+    .then(response => response.json())
+    .then(data => {
+      const wrapper_list = document.getElementById("list-rec1");
+      const title = document.createElement("h1");
+      title.innerHTML = data.category;
+      wrapper_list.appendChild(title);
+      const list = document.createElement("ul");
+      data.requests.forEach(request => {
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = request.url;
+        const img = document.createElement("img");
+        img.src = request.img;
+        const textContent = document.createElement("div");
+        textContent.innerHTML = `${request.name}<br><br>${request.detail}`;
+        li.appendChild(img);
+        link.appendChild(textContent);
+        li.appendChild(link);
+        list.appendChild(li);
+      });
+      wrapper_list.appendChild(list);
+    });
 
-                li.appendChild(img);
-                li.appendChild(textContent);
-                list.appendChild(li);
-            });
-            wrapper_list.appendChild(list);
+         fetch("clothes_requests_list.json")
+         .then(response => response.json())
+         .then(data => {
+           const wrapper_list = document.getElementById("list-rec2");
+           const title = document.createElement("h1");
+           title.innerHTML = data.category;
+           wrapper_list.appendChild(title);
+           const list = document.createElement("ul");
+           data.requests.forEach(request => {
+             const li = document.createElement("li");
+             const link = document.createElement("a");
+             link.href = request.url;
+             const img = document.createElement("img");
+             img.src = request.img;
+             const textContent = document.createElement("div");
+             textContent.innerHTML = `${request.name}<br><br>${request.detail}`;
+             li.appendChild(img);
+             link.appendChild(textContent);
+             li.appendChild(link);
+             list.appendChild(li);
+           });
+           wrapper_list.appendChild(list);
          });
+
 }
+
+
